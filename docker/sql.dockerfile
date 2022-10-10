0172a2a10e9f13db0e8f9da6eb10116948b64216
Daniel Robinson <dan7c2@gmail.com>
2022-10-10 19:24:06 +0100

    add in docker and scripts

---
+++ docker/sql.dockerfile
@@ -0,0 +1,15 @@
+FROM mariadb:10.9.2 AS builder
+
+# That file does the DB initialization but also runs mysql daemon, by removing the last line it will only init
+RUN ["sed", "-i", "s/exec \"$@\"/echo \"not running $@\"/", "/usr/local/bin/docker-entrypoint.sh"]
+
+ENV MYSQL_ROOT_PASSWORD=root
+
+# generic sqls for user pkg tests
+COPY sql/dnsk.sql /docker-entrypoint-initdb.d/dnsk.sql
+
+RUN ["/usr/local/bin/docker-entrypoint.sh", "mysqld", "--datadir", "/initialized-db", "--aria-log-dir-path", "/initialized-db"]
+
+FROM mariadb:10.9.2
+
+COPY --from=builder /initialized-db /var/lib/mysql
---
+++ sql/dnsk.sql
@@ -0,0 +1,48 @@
+DROP DATABASE IF EXISTS Dnsk;
+CREATE DATABASE Dnsk
+CHARACTER SET = 'utf8mb4'
+COLLATE = 'utf8mb4_general_ci';
+USE Dnsk;
+
+DROP TABLE IF EXISTS Auths;
+CREATE TABLE Auths (
+                       Id BINARY(16) NOT NULL,
+                       Email VARCHAR(250) NULL,
+                       Phone VARCHAR(20) NULL,
+                       RegisteredOn DATETIME(3) NOT NULL,
+                       ActivatedOn DATETIME(3) NOT NULL,
+                       ActivateCode VARCHAR(250) NULL,
+                       NewEmail VARCHAR(250) NULL,
+                       NewEmailCode VARCHAR(250) NULL,
+                       NewPhone VARCHAR(20) NULL,
+                       NewPhoneCode VARCHAR(250) NULL,
+                       LoginCodeCreatedOn DATETIME(3) NULL,
+                       LoginCode VARCHAR(250) NULL,
+                       Use2FA       BOOLEAN NOT NULL,
+                       ScryptSalt   VARBINARY(256) NULL,
+                       ScryptPwd    VARBINARY(256) NULL,
+                       ScryptN      MEDIUMINT UNSIGNED NULL,
+                       ScryptR      MEDIUMINT UNSIGNED NULL,
+                       ScryptP      MEDIUMINT UNSIGNED NULL,
+                       PRIMARY KEY Id (Id),
+                       UNIQUE INDEX Email (Email),
+                       UNIQUE INDEX Phone (Phone),
+                       INDEX(ActivatedOn, RegisteredOn)
+);
+
+# cleanup old registrations that have not been activated in a week
+SET GLOBAL event_scheduler=ON;
+DROP EVENT IF EXISTS authRegistrationCleanup;
+DROP EVENT IF EXISTS AuthRegistrationCleanup;
+CREATE EVENT AuthRegistrationCleanup
+ON SCHEDULE EVERY 24 HOUR
+STARTS CURRENT_TIMESTAMP + INTERVAL 1 HOUR
+DO DELETE FROM Auths WHERE ActivatedOn=CAST('0001-01-01 00:00:00.000' AS DATETIME(3)) AND RegisteredOn < DATE_SUB(NOW(), INTERVAL 7 DAY);
+
+DROP USER IF EXISTS 'Dnsk'@'%';
+CREATE USER 'Dnsk'@'%' IDENTIFIED BY 'C0-Mm-0n-Dnsk';
+GRANT SELECT ON Dnsk.* TO 'Dnsk'@'%';
+GRANT INSERT ON Dnsk.* TO 'Dnsk'@'%';
+GRANT UPDATE ON Dnsk.* TO 'Dnsk'@'%';
+GRANT DELETE ON Dnsk.* TO 'Dnsk'@'%';
+GRANT EXECUTE ON Dnsk.* TO 'Dnsk'@'%';
