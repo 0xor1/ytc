8196234c9f7159e64ad9b71f7ccedf84e5078e74
Daniel Robinson <dan7c2@gmail.com>
2022-12-12 21:21:15 +0000

    add to di container

--- Client/Program.cs
+++ Client/Program.cs
@@ -12,6 +12,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddSingleton<IAuthService, AuthService>();
 builder.Services.AddSingleton<IMainLayoutService, MainLayoutService>();
 builder.Services.AddSingleton<ErrorInterceptor>();
 builder.Services.AddSingleton(services => 
