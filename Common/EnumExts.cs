8cbab88ec65dc9877cea39433d1675080746c46c
Daniel Robinson <dan7c2@gmail.com>
2022-12-21 09:24:09 +0000

    upgrade to dotnet7 and start removing custom ui components in favour of radzen ui library

---
+++ Common/EnumExts.cs
@@ -0,0 +1,10 @@
+﻿
+using Humanizer;
+
+namespace Dnsk.Common;
+
+public static class EnumExts
+{
+    public static string CssClass(this Enum input)
+        => input.Humanize(LetterCasing.LowerCase).Replace(" ", "");
+}
