b1821ceded38e4d2a713fc6eba080b6d17d65e08
Daniel Robinson <dan7c2@gmail.com>
2022-12-21 10:17:12 +0000

    move to radzen

--- Client/Lib/MainLayoutService.cs
+++ Client/Lib/MainLayoutService.cs
@@ -13,17 +13,9 @@ public enum ToastLevel
 
 public record Toast(ToastLevel Level, string Message);
 
-public enum Theme
-{
-    Light,
-    Dark,
-    // ColorBlind
-}
 public interface IMainLayout
 {
-    void Left(bool? show = null);
     void PopToast(Toast t);
-    void SetTheme(Theme t);
 }
 public interface IMainLayoutService : IMainLayout
 {
@@ -40,8 +32,6 @@ public class MainLayoutService: IMainLayoutService
         _impl ??= i;
     }
 
-    public void Left(bool? show = null) => _impl?.Left(show);
     public void PopToast(Toast t) => _impl?.PopToast(t);
-    public void SetTheme(Theme t) => _impl?.SetTheme(t);
 
 }
