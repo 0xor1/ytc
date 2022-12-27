8a547e96f34227d9a20a3b43ec76a61f869f3041
Daniel Robinson <dan7c2@gmail.com>
2022-12-27 20:18:04 +0000

    move service setup to service pkg

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,26 +1,8 @@
-using Dnsk.Db;
 using Dnsk.Service.Services;
 using Dnsk.Service.Util;
-using Microsoft.EntityFrameworkCore;
-using MySqlConnector;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddGrpc(opts =>
-{
-    opts.Interceptors.Add<ErrorInterceptor>();
-});
-builder.Services.AddScoped<ISessionManager, SessionManager>();
-builder.Services.AddDbContext<DnskDb>(
-    dbContextOptions =>
-    {
-        var cnnStrBldr = new MySqlConnectionStringBuilder(Config.Db.Connection);
-        cnnStrBldr.Pooling = true;
-        cnnStrBldr.MaximumPoolSize = 100;
-        cnnStrBldr.MinimumPoolSize = 1;
-        var version = new MariaDbServerVersion(new Version(10, 8));
-        dbContextOptions
-            .UseMySql(cnnStrBldr.ToString(), version, opts => { opts.CommandTimeout(1); });
-    });
+builder.Services.AddApiServices();
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
---
+++ Service/Util/ServiceExts.cs
@@ -0,0 +1,29 @@
+﻿using Dnsk.Db;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MySqlConnector;
+
+namespace Dnsk.Service.Util;
+
+public static class ServiceExts
+{
+    public static void AddApiServices(this IServiceCollection services)
+    {
+        services.AddGrpc(opts =>
+        {
+            opts.Interceptors.Add<ErrorInterceptor>();
+        });
+        services.AddScoped<ISessionManager, SessionManager>();
+        services.AddDbContext<DnskDb>(
+            dbContextOptions =>
+            {
+                var cnnStrBldr = new MySqlConnectionStringBuilder(Config.Db.Connection);
+                cnnStrBldr.Pooling = true;
+                cnnStrBldr.MaximumPoolSize = 100;
+                cnnStrBldr.MinimumPoolSize = 1;
+                var version = new MariaDbServerVersion(new Version(10, 8));
+                dbContextOptions
+                    .UseMySql(cnnStrBldr.ToString(), version, opts => { opts.CommandTimeout(1); });
+            });
+    }
+}
