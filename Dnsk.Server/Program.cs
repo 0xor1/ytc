af936d4c31295c3bc57e84564d23691d7e71425d
Daniel Robinson <dan7c2@gmail.com>
2023-03-18 11:47:10 +0000

    pull out server startup code

--- Dnsk.Server/Program.cs
+++ Dnsk.Server/Program.cs
@@ -1,25 +1,6 @@
-using Dnsk.Service.Services;
 using Common.Server;
+using Dnsk.Service.Services;
 using Dnsk.Db;
 using Dnsk.I18n;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddApiServices<DnskDb>(S.UnexpectedError);
-
-var app = builder.Build();
-if (app.Environment.IsDevelopment())
-{
-    app.UseWebAssemblyDebugging();
-}
-else
-{
-    app.UseHsts();
-}
-app.UseHttpsRedirection();
-app.UseBlazorFrameworkFiles();
-app.UseStaticFiles();
-app.UseRouting();
-app.UseGrpcWeb(new GrpcWebOptions { DefaultEnabled = true });
-app.MapGrpcService<ApiService>();
-app.MapFallbackToFile("index.html");
-app.Run(Config.Server.Listen);
+Server.Run<DnskDb, ApiService>(args, S.UnexpectedError);
