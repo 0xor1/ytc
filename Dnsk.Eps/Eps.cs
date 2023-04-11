c2193b73e584a6510287b9851b2bea02df9a3dc2
Daniel Robinson <dan7c2@gmail.com>
2023-04-11 09:02:10 +0100

    tweaks for rpc

---
+++ Dnsk.Eps/Eps.cs
@@ -0,0 +1,11 @@
+﻿using Common.Server;
+using Common.Server.Auth;
+using Dnsk.Db;
+
+namespace Dnsk.Eps;
+
+public static class DnskEps
+{
+    private static IReadOnlyList<IRpcEndpoint>? _eps;
+    public static IReadOnlyList<IRpcEndpoint> Eps => _eps ??= AuthEps<DnskDb>.Eps;
+}
