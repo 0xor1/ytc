c6741995ac3f00497b046389511865086565885c
Daniel Robinson <dan7c2@gmail.com>
2023-04-13 21:25:12 +0100

    get rid of radzen suffix

--- Dnsk.Client/Program.cs
+++ Dnsk.Client/Program.cs
@@ -1,7 +1,7 @@
 using Common.Client;
 using Common.Shared.Auth;
 using Dnsk.Api;
-using Dnsk.Client.Rz;
+using Dnsk.Client;
 using Dnsk.I18n;
 
 await Client.Run<App, Dnsk.Api.IApi>(args, S.Inst, Dnsk.Api.IApi.Init());
